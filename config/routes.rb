9c27095ec472e61b33b1c1c3f295e9bb2ef1fbdf
Jacob Mziya <jacobmziya@gmail.com>
2019-07-11 10:41:15 +0200

    Added case listing table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'settings/index'
-  get 'dashboard/index'
+  get 'my_account/index'
+  get '/settings', to: 'settings#index'
+  get '/dashboard', to: 'dashboard#index'
   get 'users/index'
   get '/add_user', to: 'users#new'
   get '/view_users', to: 'users#show'
