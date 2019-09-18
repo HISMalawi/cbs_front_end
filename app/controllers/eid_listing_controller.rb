c3a39722bfefe74ef4681769dd6d2ebede83f3d3
Matthew Mvula <matthewmvula1@gmail.com>
2019-09-18 15:57:46 +0200

    Added New Date Format In Both Reports

--- app/controllers/eid_listing_controller.rb
+++ app/controllers/eid_listing_controller.rb
@@ -16,7 +16,7 @@ class EidListingController < ApplicationController
       data = []
 
       (JSON.parse(response) || []).each do |sn, resp|
-        birth_month = resp['birthdate'].to_date.strftime("%m-%Y")
+        birth_month = resp['birthdate'].to_date.strftime("%b/%Y")
 
         data << [
             sn,
