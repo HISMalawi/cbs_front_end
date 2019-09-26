30e99bb4fae84cde06244ec577e11ab28008d22e
Jacob Mziya <jacobmziya@gmail.com>
2019-09-26 09:41:07 +0200

    Made current facility visible.

--- app/controllers/case_listing_controller.rb
+++ app/controllers/case_listing_controller.rb
@@ -33,6 +33,7 @@ class CaseListingController < ApplicationController
             resp['hiv_test_facility'],
             resp['initiation_date'],
             resp['current_regimen'],
+            resp['current_facility'],
             resp['latest_vl_result'],
             resp['latest_vl_date'],
             resp['latest_vl_facility']
